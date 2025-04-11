95040d924849c1d4b7ebc4187e98ed1738fa7b97
Asya-sun <asyasunsea@gmail.com>
2025-04-11 16:31:48 +0700

    feat: little change

--- quadratic_solver/quadratic_solver.py
+++ quadratic_solver/quadratic_solver.py
@@ -6,7 +6,7 @@ class QuadraticEquationSolver:
         if a == 0:
             if b == 0:
                 return None  # Уравнение 0 = c (нет решений или бесконечно много)
-            return (-c / b,)  # Линейное уравнение bx + c = 0
+            return (-c / b,)  # Линейное уравнение bx + c = 0;
         discriminant = b**2 - 4*a*c
 
         if discriminant < 0:
